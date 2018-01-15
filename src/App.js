c934d2fa897c5cdc4309a354627e98fcc29c3998
Szona <cmielalicja@gmail.com>
2018-01-15 11:07:25 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+import './App.css'
+import Search from './components/Search'
+import Library from './components/Library'
+import { Route } from 'react-router-dom'
+import * as BooksAPI from './BooksAPI'
+
+
+
+class BooksApp extends Component {
+
+  state = {
+    books: null
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then(books => {
+      this.setState({ books })
+    })
+  }
+
+  updateParent = (event) => {
+    BooksAPI.getAll().then(books => {
+      this.setState({ books })
+      console.log(this.state)
+    })
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <Route exact path="/" render={() => (
+          <Library handler={this.updateParent} books={this.state.books}/>
+        )}/>
+        <Route path="/search" render={() => (
+          <Search handler={this.updateParent} books={this.state.books}/>
+        )}/>
+      </div>
+    )
+  }
+}
+
+export default BooksApp
